10f1f2e7a558cf8b09deb7edb9c9e14edc9c52b9
AlainDevis <devisalain04@gmail.com>
2020-12-02 08:25:27 +0200

    creating the error handler

--- app/__init__.py
+++ app/__init__.py
@@ -6,7 +6,7 @@ bootstrap = Bootstrap()
 
 def create_app(config_name):
     app = Flask(__name__)
-    # Creating the app configurations
+
     app.config.from_object(config_options[config_name])
 
     bootstrap.init_app(app)
